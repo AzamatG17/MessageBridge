ece74d52c204d73cc0e94a4c3916dac5cad948e6
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-08-27 17:17:13 +0500

    Improve Email Sender

--- MessageBridge/MessageBridge/Services/EmailSender.cs
+++ MessageBridge/MessageBridge/Services/EmailSender.cs
@@ -1,17 +1,27 @@
 ﻿using System.Net.Mail;
 using System.Net;
+using MessageBridge.Models.Exceptions;
+using Xeptions;
+using MessageBridge.Interfaces;
 
 namespace MessageBridge.Services
 {
     public class EmailSender : Interfaces.IEmailSender
     {
+        private readonly ILogging _loggingBroker;
+
+        public EmailSender(ILogging loggingBroker)
+        {
+            _loggingBroker = loggingBroker;
+        }
+
         public async Task<bool> SendMessage(string email, string massage, string subject)
         {
             using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
             {
                 try
                 {
-                    MailAddress fromAddress = new MailAddress("gieosovazamat@gmail.com", "EasyQ");
+                    MailAddress fromAddress = new MailAddress("barakatexnika.uz@gmail.com", subject);
                     MailAddress toAddress = new MailAddress(email);
                     MailMessage mailMessage = new MailMessage(fromAddress, toAddress)
                     {
@@ -21,23 +31,29 @@ namespace MessageBridge.Services
 
                     smtpClient.EnableSsl = true;
                     smtpClient.UseDefaultCredentials = false;
-                    smtpClient.Credentials = new NetworkCredential("gieosovazamat@gmail.com", "dhrk hrxo qrec cedz");
+                    smtpClient.Credentials = new NetworkCredential("barakatexnika.uz@gmail.com", "dvkx kexu wbye cgud");
 
                     await smtpClient.SendMailAsync(mailMessage);
 
                     return true;
                 }
-                //catch (InvalidSendingEmail ex)
-                //{
-                //    SendMessageError(ex);
-                //    return false;
-                //}
+                catch (InvalidSendingEmail ex)
+                {
+                    SendMessageError(ex);
+                    throw;
+                }
                 catch (Exception ex)
                 {
-                    //SendMessageError(ex);
-                    return false;
+                    SendMessageError(ex);
+                    throw;
                 }
             }
+        }
+
+        private void SendMessageError(Exception ex)
+        {
+            var xeption = new InvalidSendingEmail(new Xeption(ex.Message));
+            _loggingBroker.LogError(xeption);
         }
     }
 }
